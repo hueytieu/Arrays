f8911bbdcdc60a8acfb9e6835639908a14004719
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-20 11:42:50 -0700

    did some arrays today

---
+++ Arrays.java
@@ -0,0 +1,12 @@
+
+
+public class Arrays
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String[] strArray = {"Alpha" "Bravo" "Charlie" "Delta" "Echo";
+            for (int i = 0; i < strArray.length; i++) {
+                strArray[i] = 
+            }
+    }
+}
