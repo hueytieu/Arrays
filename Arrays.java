e11fb0fcc99242b6a79a52264c745d85eec84a8b
Huey Tieu <huey.tieu@mrjaffesclass.com>
2022-09-26 11:37:25 -0700

    FHSSSSHUSHDIUSA IUAXDDDDDDD

--- Arrays.java
+++ Arrays.java
@@ -1,15 +1,16 @@
 
-
 public class Arrays
 {
     public static void main(String[] args) {
         String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
-            for (int i = 0; i < strArray.length; i++) {
-                System.out.println(strArray[i]);
-            }
-            for (String value : strArray) {
-                System.out.println(value);
-                
-            }
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+        for (String value : strArray) {
+            System.out.print(value);
+        }
+        for (String value : strArray) {
+            System.out.print(value + "-");
+        }
     }
 }
